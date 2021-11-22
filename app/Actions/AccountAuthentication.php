f5e8e19c28c146dcff2fa03c786a762545ded84c
rezky08 <rezky.setiawan85@gmail.com>
2021-11-22 12:01:09 +0700

    - add password verify

--- app/Actions/AccountAuthentication.php
+++ app/Actions/AccountAuthentication.php
@@ -1,6 +1,11 @@
 <?php
 namespace App\Actions;
+use App\Exceptions\Error;
+use App\Http\Response;
 use App\Models\User;
+use Dflydev\DotAccessData\Data;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Request;
 
 class AccountAuthentication{
@@ -11,16 +16,36 @@ class AccountAuthentication{
 
     const TOKEN_TYPE_USER_LOGIN = 'USER_LOGIN';
 
+    public User $user;
+
+    public Builder $query;
+
     public function __construct(Request $request)
     {
         $this->request = $request;
+        $this->query = User::query();
+
+    }
+
+    public function auth(){
+        try {
+            /** @var User $user */
+            $user = $this->query->where(self::COLUMN_AUTH,$this->request->get(self::COLUMN_AUTH))->firstOrFail();
+            $this->user = $user;
+        }catch (\Exception $e){
+            throw Error::make(Response::CODE_ERROR_UNAUTHENTICATED);
+        }
+
+        /** Password Check */
+        throw_if(!Hash::check($this->request->get('password'),$this->user->getAuthPassword()),Error::make(Response::CODE_ERROR_UNAUTHENTICATED));
+
     }
 
     public function attempt(){
-        $query = User::query()->where(self::COLUMN_AUTH,$this->request->get(self::COLUMN_AUTH));
-        /** @var User $user */
-        $user = $query->firstOrFail();
-        $token = $user->createToken(self::TOKEN_TYPE_USER_LOGIN);
+
+        $this->auth();
+
+        $token = $this->user->createToken(self::TOKEN_TYPE_USER_LOGIN);
 
         return [
             'token'=>$token->plainTextToken
